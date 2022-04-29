e79c9c0c27bdaf1591cb847aa33a7276dd1d3c37
marcoweinmann <marco.wm94@gmail.com>
2022-04-29 17:48:31 +0200

    Aufgaben erledigt!

---
+++ lektion4 /js/Datentypen.js
@@ -0,0 +1,22 @@
+let zahl = 8
+let string = "Hallo"
+let boolean = true & false
+let object = {
+  name: "Martin Richter",
+  alter: 32,
+  groeße: 1.82
+
+}
+console.debug(object)
+function heute() {
+  let ampel = "gruen"
+
+  if (ampel == "gruen") {
+    console.debug("Die Farbe ist grün", true)
+  }
+}
+heute()
+//let _function = function heute () {
+  //console.debug(heute, true)
+//}
+
