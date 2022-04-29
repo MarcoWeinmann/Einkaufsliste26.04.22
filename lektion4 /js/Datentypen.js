786f708b8a59a1a71b5be0af1e94b881ff46530b
marcoweinmann <marco.wm94@gmail.com>
2022-04-29 18:22:08 +0200

    Aufgaben erledigt!

--- lektion4 /js/Datentypen.js
+++ lektion4 /js/Datentypen.js
@@ -1,6 +1,23 @@
+/**         Es gibt 5 DATENTYPEN
+ * */
+
+
+/** Datentyp 1: Ist eine Zahl (z.B. 8 )**/
+
 let zahl = 8
-let string = "Hallo"
+
+/** Datentyp 2: Ist ein string (ist immer ein text und wird mit "" ) gekennzeichnet (z.B. Hallo Welt )**/
+
+let string = "Hallo Welt"
+
+/** Datentyp 3: Ist ein Boolean (ein Boolean ist immer die Aussage "wahr" oder "falsch") (true & false )**/
+
 let boolean = true & false
+
+/** Datentyp 4: Ist ein Object (z.B. map, array, variable )**/
+
+//let pizza = ["Tomate", "Salami", 8, true] <---  das ist ein Array [Ein Array funktioniert wie eine Liste!]
+
 let object = {
   name: "Martin Richter",
   alter: 32,
@@ -8,6 +25,9 @@ let object = {
 
 }
 console.debug(object)
+
+/** Datentyp 5: Ist eine function (eine function ist hat eine Funktion wie ein Miniprogramm) (z.B. function heute() )**/
+
 function heute() {
   let ampel = "gruen"
 
