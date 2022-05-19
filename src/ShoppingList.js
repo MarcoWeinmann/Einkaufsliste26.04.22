3bc8c7a807abb580e3612bc599b95b19e30160e6
marcoweinmann <marco.wm94@gmail.com>
2022-05-19 14:03:33 +0200

    React-Lektion 1 erledigt

--- src/ShoppingList.js
+++ src/ShoppingList.js
@@ -11,8 +11,54 @@ class ShoppingList extends React.Component {
       //füge hier deinen HTML-Code ein
       <div id="container">
         {/* ToDo: füge hier drunter Deinen HTML-Code ein */}
-        Füge hier Deinen HTML-Code ein<br/>
-        <GruppenTag/>
+     <br/>
+        <header>
+          <h1>Einkaufsliste</h1>
+          <nav>
+            <input type="text" placeholder="Artikel hinzufügen"/>
+              <button className="material-icons" >add_circle</button>
+          </nav>
+        </header>
+
+        <hr/>
+
+          <main>
+            <section>
+              <h2>Einkaufen
+                <i className="material-icons">expand_less</i>
+              </h2>
+              <dl>
+                <GruppenTag/>
+              </dl>
+            </section>
+            <hr/>
+              <section>
+                <h2>Erledigt
+                  <i className="material-icons">expand_less</i>
+                </h2>
+                <dl>
+                  <dt >Hülsenfrüchte
+                    <i className="material-icons">expand_less</i>
+                  </dt>
+                  <dd><label><input type="checkbox"/> <s>Tofu</s></label></dd>
+                </dl>
+              </section>
+          </main>
+          <hr/>
+
+            <footer>
+              <nav>
+                <button>
+                  <span className="material-icons">bookmark_add</span> GRUPPEN
+                </button>
+                <button>
+                  <span className="material-icons">sort</span> SORT
+                </button>
+                <button>
+                  <span className="material-icons">settings</span> SETUP
+                </button>
+              </nav>
+            </footer>
       </div>
     )
   }
