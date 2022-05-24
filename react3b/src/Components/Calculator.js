82be48e1825e4923e74294b881e5ba932d32c65c
marcoweinmann <marco.wm94@gmail.com>
2022-05-24 13:45:24 +0200

    react 3b aufgabe 5 erledigt

--- react3b/src/Components/Calculator.js
+++ react3b/src/Components/Calculator.js
@@ -29,7 +29,7 @@ class Calculator extends React.Component {
         let celsius, fahrenheit = temperature
 
         /* ToDO: Ersetze hier 'true' durch die in der Aufgabe beschriebene Bedingung*/
-        if(true){
+        if(scale == "f"){
             celsius = tryConvert(temperature, toCelsius)
         }else{
             fahrenheit = tryConvert(temperature, toFahrenheit)
