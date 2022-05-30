eb4f8ad94dd330e739ba8f328819469766d22939
marcoweinmann <marco.wm94@gmail.com>
2022-05-30 12:57:14 +0200

    React 4 erledigt

--- react4/src/Components/PartyButton.js
+++ react4/src/Components/PartyButton.js
@@ -11,8 +11,9 @@ class PartyButton extends React.Component{
             /*  Aufgabe 2
              * ToDo: Schreibe ein onClick Event für diesen Button, sodass die Funktion startParty() ausgeführt wird.
              */
+
             <div>
-                 <button>START PARTY</button>
+                 <button onClick={this.startParty}>START PARTY</button>
             </div>
         )
     }
